b3667495ae103b9c1d7d03c70456394d3de1d341
Devieswaran <devi.eswaran98@gmail.com>
2017-06-30 16:31:10 +0530

    Update aa.c

--- aa.c
+++ aa.c
@@ -1,13 +1,2 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
-{
-clrscr();
-printf("\n hello");
-printf("\n hello");
-printf("\n hello");
-printf("\n hello");
-printf("\n hello");
-getch();
-return 0;
-}
