b6a8395a9fc60a4ed4928da1b3453fff574ce46f
Devieswaran <devi.eswaran98@gmail.com>
2017-06-30 15:40:33 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("\n Enter the number");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\n Even");
+}
+else
+{
+printf("odd");
+}
+return 0;
+}
